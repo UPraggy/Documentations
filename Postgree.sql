1182245c22967b2d51a8ae9188f17e526b55d25b
Rafael Moreira Ramos de Rezende <100146657+UPraggy@users.noreply.github.com>
2023-06-22 12:54:33 -0300

    Update Postgree.sql

--- Postgree.sql
+++ Postgree.sql
@@ -51,12 +51,12 @@ insert into funcionarios values
 (1,'Kelley','rkelley0@soundcloud.com','Feminino','Automotivo','10/2/2009',67470,'Structural Engineer',2),
 (2,'Armstrong','sarmstrong1@infoseek.co.jp','Masculino','Bebês','3/31/2008',71869,'Financial Advisor',2),
 (3,'Carr','fcarr2@woothemes.com','Masculino','Automotivo','7/12/2009',101768,'Recruiting Manager',1),
-(4,'Murray','jmurray3@gov.uk','Feminino','Bebês','12/25/2014',96897,'Desktop Support Technician',2),
+(4,'Murray','jmurray3@gov.uk','Feminino','Bebês','12/25/2014',67470,'Desktop Support Technician',2),
 (5,'Ellis','jellis4@sciencedirect.com','Feminino','Beleza','9/19/2002',63702,'Software Engineer III',1),
 (6,'Phillips','bphillips5@time.com','Masculino','Roupas','8/21/2013',118497,'Executive Secretary',1),
-(7,'Williamson','rwilliamson6@ted.com','Masculino','Beleza','5/14/2006',65889,'Dental Hygienist',2),
-(8,'Harris','aharris7@ucoz.com','Feminino','Roupas','8/12/2003',84427,'Safety Technician I',1);
-
+(7,'Williamson','rwilliamson6@ted.com','Masculino','Beleza','5/14/2006',67470,'Dental Hygienist',2),
+(8,'Harris','aharris7@ucoz.com','Feminino','Roupas','8/12/2003',84427,'Safety Technician I',1),
+(9,'fesf','fsef@ucoz.com','Feminino','Roupas','8/12/2003',84427,'Safety Technician I',1);
 
 -- ############################# CONTANDO E OPERACOES #############################
 -- Contando
@@ -79,11 +79,33 @@ order by salario desc;
 
 -- retornando Media
 
-select round(avg(salario),1) from funcionarios;
+select round(avg(salario),1) as "MEDIA" from funcionarios;
 
 
 -- RETORNANDO SOMA
 select sum(salario) from funcionarios;
+
+-- MODA
+select salario, count(salario) as "MODA" from funcionarios
+group by salario
+order by 2 desc
+limit 3;
+
+-- AMPLITUDE
+
+select max(salario) - min(salario) as "AMPLITUDE" from funcionarios;
+
+-- DESVIO PADRAO E VARIANCIA
+
+select round(stddev_pop(salario),2) as "DESVIO PADRAO",
+round(var_pop(salario),2) as "VARIANCIA"
+from funcionarios;
+
+-- MEDIANA google
+
+
+
+
 
 
 -- ############################# LIMITANDO LINHAS ####################
